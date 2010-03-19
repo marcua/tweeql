566cb2ac113a032446a4fd515577ce4d5198339a
Adam Marcus <marcua@marcua.net>
2010-03-19 15:04:25 -0400

    Adding functional db bulkloader

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ REDIS_HOSTNAME = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
 
-POLL_TIME = 1 # seconds between looking for new content in redis
+POLL_TIME = .25 # seconds between looking for new content in redis
 MAX_MESSAGES = 10000
 
 TWEET_GROUP_ID = 'tweet-group-id'
