4222c2b16cea4b2a64d6af190fabd68bb2d9a025
marcua <marcua@marcua.net>
2010-03-19 10:26:11 -0400

    Adding some DB functionality

--- streambulkloader.py
+++ streambulkloader.py
@@ -14,7 +14,7 @@ from tweepy.utils import convert_to_utf8_str
 api = API()
 json = import_simplejson()
 
-def bulk_load(tweets):
+def bulk_load(tweets, db):
 #    with tempfile.NamedTemporaryFile(mode='w') as tmpfile:
     with open('/tmp/testtweets', 'w') as tmpfile:
         writer = csv.writer(tmpfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
@@ -22,25 +22,30 @@ def bulk_load(tweets):
         for jsontweet in tweets:
             tweet = Status.parse(api, json.loads(jsontweet))
             retweeted = (getattr(tweet, 'retweeted_status', None) != None)
-            writer.writerow([tweet.author.screen_name, tweet.created_at, convert_to_utf8_str(tweet.text), retweeted])
+            writer.writerow([tweet.author.id, tweet.created_at, convert_to_utf8_str(tweet.text), retweeted])
             # TODO: geolocation
         tmpfile.flush()
         print "filled %s" % (tmpfile.name)
-        copy_cmd = "COPY tweets ( screen_name, created_at, text, retweeted ) FROM '%s' WITH DELIMITER AS ',' CSV QUOTE AS '\"'"
-        copy_cmd = copy_cmd % (tmpfile.name)
+        copy_cmd = "COPY tweets (author_id, created_at, text, retweeted) FROM STDIN WITH DELIMITER AS ',' CSV QUOTE AS '\"'"
+        cur = db.cursor()
+        cur.copy_expert(copy_cmd, tmpfile)
+        
+        
 
-def poll_data(r):
+def poll_data(r, db):
     listkey = r.lindex(settings.TO_INDEX, -1)#r.rpoplpush(settings.TO_INDEX, settings.CONSUMED_INDICES)
     if listkey != None:
         tweets = r.lrange(listkey, 0, -1)
-        bulk_load(tweets)
+        bulk_load(tweets, db)
         #r.lrem(CONSUMED_INDICES, 0, listkey)
         #r.del(listkey)
 
 def main():
     r = redis.Redis(host=settings.REDIS_HOSTNAME, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+    conn_str = "dbname='%s' user='%s' host='%s' password='%s'" % (settings.DATABASE_NAME, settings.DATABASE_USERNAME, settings.DATABASE_HOST, settings.DATABASE_PASSWORD)
+    db = psycopg2.connect(conn_str);
     while True:
-        poll_data(r)
+        poll_data(r, db)
         time.sleep(settings.POLL_TIME)
 
 if __name__ == '__main__':
