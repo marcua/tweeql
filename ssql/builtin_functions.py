af89fffb3293bfbbbdb54ae0bc687ef91971b5c3
marcua <marcua@marcua.net>
2010-07-15 12:37:47 -0400

    Adding outlier detector (calculates how many stdevs you are from the mean) and a floor function

--- ssql/builtin_functions.py
+++ ssql/builtin_functions.py
@@ -7,6 +7,7 @@ from geopy import geocoders
 from ordereddict import OrderedDict
 from urllib2 import URLError
 
+import math
 import re
 import sys
 
@@ -34,6 +35,18 @@ class Temperature():
                 temperature = ((9.0/5) * temperature) + 32
         return temperature
 
+class Rounding():
+    return_type = ReturnType.FLOAT
+
+    @staticmethod
+    def floor(tuple_data, val, nearest = 1):
+        """ 
+            Returns the largest integer multiple of 'nearest' that is less than or equal to val.
+            If nearest is less than 1, you may see funny results because of floating 
+            point arithmetic.
+        """
+        retval = val - (val % nearest) if val != None else None
+        return retval
 
 class Location:
     class LruDict(OrderedDict):
@@ -93,7 +106,68 @@ class Location:
             print "Unable to connect to GeoNames: %s" % (e)
         return latlng
 
+class NormalOutliers():
+    return_type = ReturnType.FLOAT
+    groups = dict()
+
+    class StdGroup():
+        def __init__(self):
+            self.n = 0
+            self.mean = 0.0
+            self.M2 = 0.0
+            self.ewma = 0.0
+            self.stdev = 0.0
+        def update_and_calculate(self, value):
+            """
+                Returns the number of standard deviations from the EWMA if
+                the number of values previously recorded is >= 5.  Otherwise,
+                returns -1.
+                If the value is less than 2 standard deviations away, it will
+                also update the value as if it is not an outlier.
+            """
+            retval = -1
+            if self.n >= 5: # only calculate stdevs if collected > 5 data pts.
+                diff = self.ewma - value
+                if self.stdev > 0:
+                    stdevs = diff/self.stdev
+                else:
+                    stdevs = diff/.00001
+                retval = abs(stdevs)
+            if retval <= 2: # only update if less than 2 stdevs or still collecting data
+                self.n += 1
+                delta = value - self.mean
+                self.mean += delta/self.n
+                self.M2 += delta*(value - self.mean)
+                if self.n > 1:
+                    self.stdev = math.sqrt(self.M2/(self.n - 1))
+                    self.ewma = (.15*value) + (.85*self.ewma)
+                else:
+                    self.ewma = value
+            return retval 
+
+    @staticmethod
+    def numstdevs(tuple_data, value, *group):
+        """ 
+            Calculates how many standard deviations from the 
+            exponentially weighted moving average value 
+            is, given the other values that have been 
+            given for the elements of this group.
+
+            The return value will be greater than or equal to 0 
+            if it represents the number of standard deviations.
+            If it is less than 0, the group does not have enough data to
+            calculate standard deviations.
+        """
+        std_group = NormalOutliers.groups.get(group, None)
+        if std_group == None:
+            std_group = NormalOutliers.StdGroup()
+            NormalOutliers.groups[group] = std_group
+
+        return std_group.update_and_calculate(value)
+
 def register_default_functions():
     fr = FunctionRegistry()
     fr.register("temperatureF", FunctionInformation(Temperature.temperature_f, Temperature.return_type))
     fr.register("tweetLatLng", FunctionInformation(Location.get_latlng, Location.return_type))
+    fr.register("floor", FunctionInformation(Rounding.floor, Rounding.return_type))
+    fr.register("normalStdevs", FunctionInformation(NormalOutliers.numstdevs, NormalOutliers.return_type))
