9a0e0cdaef9ed919c396b6e3a56025caa67b3632
marcua <marcua@marcua.net>
2010-07-16 17:46:11 -0400

    mean deviation outlier detector and some test code

--- ssql/builtin_functions.py
+++ ssql/builtin_functions.py
@@ -165,9 +165,82 @@ class NormalOutliers():
 
         return std_group.update_and_calculate(value)
 
+class MeanOutliers():
+    return_type = ReturnType.FLOAT
+    groups = dict()
+
+    class MeanGroup():
+        def __init__(self):
+            self.n = 0
+            self.ewma = 0.0 # exponentially weighted moving avgerage
+            self.ewmmd = 0.0 # exponentially weighted moving mean deviation
+        def update_and_calculate(self, value):
+            """
+                Returns the number of mean deviations from the EWMA if
+                the number of values previously recorded is >= 5.  Otherwise,
+                returns -1.
+
+                Updates the EWMA and EWMMD after calculating how many median deviations
+                away the result is.
+            """
+            retval = -1
+            diff = abs(self.ewma - value)
+            if self.n >= 5: # only calculate meandevs if collected > 5 data pts.
+                if self.ewmmd > 0:
+                    meandevs = diff/self.ewmmd
+                else:
+                    meandevs = diff/.00001
+                retval = meandevs
+            
+            # update ewma/ewmmd
+            self.n += 1
+            if self.n > 1:
+                if self.n > 2:
+                    self.ewmmd = (.125*diff) + (.875*self.ewmmd)
+                else:
+                    self.ewmmd = diff
+                self.ewma = (.125*value) + (.875*self.ewma)
+            else:
+                self.ewma = value
+            return retval 
+
+    @staticmethod
+    def nummeandevs(tuple_data, value, *group):
+        """ 
+            Calculates how many mean deviations from the exponentially 
+            weighted moving average value is, given the other values that have been 
+            given for the elements of this group.
+
+            Uses the method TCP utilizes to estimate the round trip time 
+            and mean deviation time of a potentially congested packet:
+            http://tools.ietf.org/html/rfc2988
+
+            The return value will be greater than or equal to 0 
+            if it represents the number of mean deviations the value was away
+            from the exponentially weighted moving average.
+            If it is less than 0, the group does not have enough data to
+            calculate mean deviations.
+
+            What is a good outlier value?  If your data is normally distributed, then
+            I experimentally found that 1 mean deviation is .8 standard deviations.
+            For normally distributed data, 68% of values are within 1 standard
+            deviation, 95% of values are within 2, and nearly 100% are within 3.
+            Thus, 68% of values are within 1.25 mean deviations, 95% are within
+            2.5 mean deviations, and almost 100% are within 3.75 mean deviations.
+            A good mean deviation cutoff for legitimate values is thus in the range
+            2.5-3.75.
+        """
+        mean_group = MeanOutliers.groups.get(group, None)
+        if mean_group == None:
+            mean_group = MeanOutliers.MeanGroup()
+            MeanOutliers.groups[group] = mean_group
+
+        return mean_group.update_and_calculate(value)
+
 def register_default_functions():
     fr = FunctionRegistry()
     fr.register("temperatureF", FunctionInformation(Temperature.temperature_f, Temperature.return_type))
     fr.register("tweetLatLng", FunctionInformation(Location.get_latlng, Location.return_type))
     fr.register("floor", FunctionInformation(Rounding.floor, Rounding.return_type))
     fr.register("normalStdevs", FunctionInformation(NormalOutliers.numstdevs, NormalOutliers.return_type))
+    fr.register("meanDevs", FunctionInformation(MeanOutliers.nummeandevs, MeanOutliers.return_type))
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import ssql.builtin_functions
+import random
+from ssql.builtin_functions import MeanOutliers
+from ssql.builtin_functions import NormalOutliers
 
 if __name__ == "__main__":
 #    print(ssql.builtin_functions.Temperature.temperature_f(None, u"Adelaide: Tue 25 May 3:30 AM - Rain Shower -  Temp. 13.0\u00B0C RH 100% Wind SW (230 degrees) at 6 km/h http://bit.ly/9Vkpg4"))
@@ -7,4 +10,53 @@ if __name__ == "__main__":
 #    print(ssql.builtin_functions.Temperature.temperature_f(None, "Temp. 13.0dC RH 100% Wind SW (230 degrees) at 6 km/h http://bit.ly/9Vkpg4"))
 #    print repr(u"(\d+(\.\d+)?)\s+°C")
 #    print repr(ur"(\d+(\.\d+)?)\s*°C")
+    """
+    print MeanOutliers.nummeandevs(None, 5, 1, 2)
+    print MeanOutliers.nummeandevs(None, 6, 1, 2)
+    print MeanOutliers.nummeandevs(None, 3, 1, 2)
+    print MeanOutliers.nummeandevs(None, 2, 1, 2)
+    print MeanOutliers.nummeandevs(None, 4, 1, 2)
+    print MeanOutliers.nummeandevs(None, 4, 1, 2)
+    print MeanOutliers.nummeandevs(None, 10, 1, 2)
+    print MeanOutliers.nummeandevs(None, 40, 1, 2)
+    print MeanOutliers.nummeandevs(None, 5, 1, 2)
+    
+    print MeanOutliers.nummeandevs(None, 60, 2, 2)
+    print MeanOutliers.nummeandevs(None, 80, 2, 2)
+    print MeanOutliers.nummeandevs(None, 75, 2, 2)
+    print MeanOutliers.nummeandevs(None, 80, 2, 2)
+    print MeanOutliers.nummeandevs(None, 90, 2, 2)
+    print MeanOutliers.nummeandevs(None, 63, 2, 2)
+    print MeanOutliers.nummeandevs(None, 72, 2, 2)
+    print MeanOutliers.nummeandevs(None, 40, 2, 2)
+    print MeanOutliers.nummeandevs(None, 120, 2, 2)
+    
+    print MeanOutliers.nummeandevs(None, 90, 2, 3)
+    print MeanOutliers.nummeandevs(None, 20, 2, 3)
+    print MeanOutliers.nummeandevs(None, 21, 2, 3)
+    print MeanOutliers.nummeandevs(None, 23, 2, 3)
+    print MeanOutliers.nummeandevs(None, 25, 2, 3)
+    print MeanOutliers.nummeandevs(None, 15, 2, 3)
+    print MeanOutliers.nummeandevs(None, 20, 2, 3)
+    print MeanOutliers.nummeandevs(None, 25, 2, 3)
+    print MeanOutliers.nummeandevs(None, 25, 2, 3)
+    print MeanOutliers.nummeandevs(None, 25, 2, 3)
+    print MeanOutliers.nummeandevs(None, 60, 2, 3)
+    """
+    meansum = 0.0
+    meandevsum = 0.0
+    count = 0
+    std = 40
+    incount = 0
+    for i in range(1,10000):
+        val = random.gauss(0,std)
+        if abs(val) < 2*std:
+            incount += 1
+        meansum += 1.0*val
+        count += 1
+        meandevsum += abs(1.0*(val - meansum/count))
+        MeanOutliers.nummeandevs(None, val, 2, 3)
+    print meansum/count, meandevsum/count
+    print "incount", incount
+    print MeanOutliers.nummeandevs(None, 80, 2, 3)
     pass
