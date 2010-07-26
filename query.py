15ff23ce7c5d460c00810643ff324ac99d46c15c
marcua <marcua@marcua.net>
2010-07-26 14:43:09 -0400

    move builtin function registry

--- query.py
+++ query.py
@@ -1,12 +1,10 @@
 from ssql.exceptions import SSQLException
-from ssql.builtin_functions import register_default_functions
 from ssql.query_runner import QueryRunner
 
 import settings
 import traceback
 
 def main():
-    register_default_functions()
     runner = QueryRunner()
     try:
         while True:
