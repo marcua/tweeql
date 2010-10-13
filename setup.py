e2a3e2c01a3307bc73c36568a4cd8466bb9b489b
marcua <marcua@marcua.net>
2010-10-13 12:40:00 -0400

    command line queries now run from tweeql/bin/tweeql-command-line.py, which is installed into the system by setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name="tweeql",
       author_email="marcua@marcua.net",
       url="http://github.com/marcua/tweeql",
       packages = find_packages(),
+      scripts = ['tweeql/bin/tweeql-command-line.py'],
       install_requires = ['pyparsing>=1.5.2', 'sqlalchemy>=0.6.1', 'ordereddict', 'geopy'],
       keywords= "twitter query sql library",
       zip_safe = True)
