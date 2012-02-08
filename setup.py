9e8a22bd3752c67151aeb748a2c80e215b2a3834
marcua <marcua@marcua.net>
2012-02-08 09:12:49 -0500

    setuptools now packages sentiment.pkl.gz, and doesn't include a bunch of other directories

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name="tweeql",
       url="http://github.com/marcua/tweeql",
       packages = find_packages(),
       scripts = ['tweeql/bin/tweeql-command-line.py'],
-      package_data = { 'tweeql' : ['extras/sentiment/sentiment.pkl.gz'] },
+      package_data = { '' : ['*sentiment.pkl.gz'] },
       install_requires = ['tweepy', 'pyparsing>=1.5.2', 'sqlalchemy>=0.6.1', 'ordereddict', 'geopy'],
       keywords= "twitter query sql library",
       zip_safe = True)
