38dc6f52fd5c4ff4fd95a21f17809328a92698c7
oshi <oshcxjc@gmail.com>
2014-03-24 17:08:06 +1100

    GeoNames requires a user name
    Settings.py.template updated with the new GEONAMES_USERNAME parameter

--- tweeql/builtin_functions.py
+++ tweeql/builtin_functions.py
@@ -125,7 +125,10 @@ class Location:
             while len(self) > size:
                 self.popitem(last=False)
 
-    gn = geocoders.GeoNames()
+    from tweeql.settings_loader import get_settings
+    settings = get_settings()
+    gn = geocoders.GeoNames(country_bias=None, username=settings.GEONAMES_USERNAME, timeout=None, proxies=None)
+	
     return_type = ReturnType.FLOAT
     LATLNG = "__LATLNG"
     LAT = "lat"
